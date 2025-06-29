b9966371e061f35c6e49e679301585495b8d9b93
Yeldos Kozhabay <yeldos.kozha@gmail.com>
2025-06-29 21:44:47 +0500

    Modify drizzle migration file 10 due to the migration issues to PostgreSQL

--- drizzle/0010_red_zarek.sql
+++ drizzle/0010_red_zarek.sql
@@ -1,12 +1,13 @@
 ALTER TABLE "posts" RENAME TO "videos";--> statement-breakpoint
+ALTER TABLE "comments" DROP CONSTRAINT "comments_post_posts_id_fk";
 ALTER TABLE "videos" DROP CONSTRAINT "posts_id_unique";--> statement-breakpoint
 ALTER TABLE "videos" DROP CONSTRAINT "posts_public_key_unique";--> statement-breakpoint
 ALTER TABLE "videos" DROP CONSTRAINT "posts_storage_key_unique";--> statement-breakpoint
 ALTER TABLE "videos" DROP CONSTRAINT "posts_thumbnail_storage_key_unique";--> statement-breakpoint
-ALTER TABLE "comments" DROP CONSTRAINT "comments_post_posts_id_fk";
 --> statement-breakpoint
 ALTER TABLE "videos" DROP CONSTRAINT "posts_author_users_id_fk";
 --> statement-breakpoint
+ALTER TABLE "videos" ADD CONSTRAINT "videos_id_unique" UNIQUE("id");--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "comments" ADD CONSTRAINT "comments_post_videos_id_fk" FOREIGN KEY ("post") REFERENCES "public"."videos"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
@@ -19,7 +20,6 @@ EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
-ALTER TABLE "videos" ADD CONSTRAINT "videos_id_unique" UNIQUE("id");--> statement-breakpoint
 ALTER TABLE "videos" ADD CONSTRAINT "videos_public_key_unique" UNIQUE("public_key");--> statement-breakpoint
 ALTER TABLE "videos" ADD CONSTRAINT "videos_storage_key_unique" UNIQUE("storage_key");--> statement-breakpoint
 ALTER TABLE "videos" ADD CONSTRAINT "videos_thumbnail_storage_key_unique" UNIQUE("thumbnail_storage_key");
