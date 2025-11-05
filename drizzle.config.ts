48225d718e3d8f4be7277f7370866dba60962c76
Yeldos Kozhabay <yeldos.kozha@gmail.com>
2025-11-05 22:54:42 +0500

    fix schema path in drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./src/db/models",
+  schema: "./dist/src/db/models",
   dbCredentials: {
     url: process.env["DATABASE_URL"] || "",
   },
