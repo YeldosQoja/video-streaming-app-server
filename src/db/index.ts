5df4e69cdd6acaff69e221f80b3939958d8b3e60
Yeldos Kozhabay <yeldos.kozha@gmail.com>
2025-12-14 11:58:38 +0500

    feat: feeding all the schemas into schema property to get type inference when using relational queries

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,30 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
+import * as categoriesSchema from "./schema/categories.sql.js";
+import * as commentsSchema from "./schema/comments.sql.js";
+import * as playlistsSchema from "./schema/playlists.sql.js";
+import * as relationsSchema from "./schema/relations.js";
+import * as tagsSchema from "./schema/tags.sql.js";
+import * as usersSchema from "./schema/users.sql.js";
+import * as videosSchema from "./schema/videos.sql.js";
+import * as videosToPlaylistsSchema from "./schema/videosToPlaylists.sql.js";
+import * as videosToTagsSchema from "./schema/videosToTags.sql.js";
 
 export const pool = new pg.Pool({
-    connectionString: process.env["DATABASE_URL"],
+  connectionString: process.env["DATABASE_URL"],
 });
 
 export const db = drizzle(pool, {
-    schema: {
-        casing: "snake_case",
-    },
+  schema: {
+    ...categoriesSchema,
+    ...commentsSchema,
+    ...playlistsSchema,
+    ...relationsSchema,
+    ...tagsSchema,
+    ...usersSchema,
+    ...videosSchema,
+    ...videosToPlaylistsSchema,
+    ...videosToTagsSchema,
+  },
 });
