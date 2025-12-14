510d76e294bebaf35df73f4ca4f28eb16a279096
Yeldos Kozhabay <yeldos.kozha@gmail.com>
2025-12-14 11:51:26 +0500

    chore: rename folder for db tables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./dist/db/models",
+  schema: "./dist/db/schema/*",
   dbCredentials: {
     url: process.env["DATABASE_URL"] || "",
   },
