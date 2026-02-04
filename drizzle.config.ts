85bef3630eada7e9826fdab52eb679b1466fc5bf
Yeldos Kozhabay <yeldos.kozha@gmail.com>
2026-02-04 23:45:28 +0500

    refactor: tidy up db credentials by dropping db url field

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "drizzle-kit";
 
+type SSLMode = "require" | "allow" | "prefer" | "verify-full";
+
 export default defineConfig({
   dialect: "postgresql",
   schema: "./dist/db/schema/*",
   dbCredentials: {
-    url: process.env["DATABASE_URL"] || "",
+    host: process.env["DB_HOST"] || "",
+    port: parseInt(process.env["DB_PORT"] || "5432"),
+    user: process.env["DB_USER"] || "",
+    database: process.env["DB_NAME"] || "",
+    password: process.env["DB_PASSWORD"] || "secret",
+    ssl: (process.env["DB_SSL"] as SSLMode) || "prefer",
   },
 });
